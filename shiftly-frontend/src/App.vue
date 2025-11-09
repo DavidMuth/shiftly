2955313ad54ae916383e6c733074994ac486ed03
MrBierflasche <schmidtmoritz143@gmail.com>
2025-11-09 14:46:55 +0100

    side bar hinzugefügt

--- shiftly-frontend/src/App.vue
+++ shiftly-frontend/src/App.vue
@@ -1,21 +1,53 @@
 <script setup lang="ts">
 import { RouterLink, RouterView } from 'vue-router'
 // @ts-ignore
-import ThemeSwitch from './components/ThemeSwitch.vue';
+import NavBar from './components/NavBar.vue';
+import { useTheme } from 'vuetify'
+
+const theme = useTheme()
+
+function toggleTheme() {
+  // Switch between 'light' and 'dark'
+  if (theme.name.value === 'dark')
+    theme.change('light')
+  else{theme.change('dark')}
+
+}
 </script>
 
 <template>
-  <v-app style="">
-    <header>
-      <div class="wrapper">
-        <nav>
-          <RouterLink to="/">Home</RouterLink>
-          <RouterLink to="/about">About</RouterLink>
-          <ThemeSwitch />
-        </nav>
-      </div>
-    </header>
+  <v-app>
+    <!-- Navbar / AppBar -->
+    <v-app-bar app color="primary" dark>
+      <v-toolbar-title>Meine App</v-toolbar-title>
+      <v-spacer></v-spacer>
+       <v-btn
 
-    <RouterView />
-    </v-app>
+      @click="toggleTheme"
+    >
+      Toggle {{ theme.name.value === 'light' ? 'Dark' : 'Light' }} Mode
+    </v-btn>
+    </v-app-bar>
+
+
+
+    <!-- Sidebar -->
+    <NavBar />
+
+    <!-- Hauptinhalt -->
+    <v-main>
+      <v-container class="py-10">
+        <h1>Willkommen!</h1>
+        <p>Hier kommt der Hauptinhalt.</p>
+      </v-container>
+    </v-main>
+
+    <!-- Footer -->
+    <v-footer app padless>
+      <v-col class="text-center" cols="12">
+        &copy; 2025 Meine App
+      </v-col>
+    </v-footer>
+  </v-app>
 </template>
+
---
+++ shiftly-frontend/src/components/NavBar.vue
@@ -0,0 +1,30 @@
+<template>
+  <v-navigation-drawer
+    expand-on-hover
+    permanent
+    rail
+    app
+  >
+    <!-- User Info -->
+    <v-list>
+      <v-list-item
+        prepend-avatar="https://randomuser.me/api/portraits/women/85.jpg"
+        subtitle="sandra_a88@gmail.com"
+        title="Sandra Adams"
+      ></v-list-item>
+    </v-list>
+
+    <v-divider></v-divider>
+
+    <!-- Navigation -->
+    <v-list density="compact" nav>
+      <v-list-item prepend-icon="mdi-folder" title="My Files" value="myfiles"></v-list-item>
+      <v-list-item prepend-icon="mdi-account-multiple" title="Shared with me" value="shared"></v-list-item>
+      <v-list-item prepend-icon="mdi-star" title="Starred" value="starred"></v-list-item>
+    </v-list>
+  </v-navigation-drawer>
+</template>
+
+<script setup lang="ts">
+// Keine zusätzliche Logik nötig
+</script>
