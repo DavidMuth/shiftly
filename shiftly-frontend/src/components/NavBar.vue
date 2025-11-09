68a6f7ef27056e0550a9355add8c90fed713bc8b
MrBierflasche <schmidtmoritz143@gmail.com>
2025-11-09 15:23:25 +0100

    nav to link router

--- shiftly-frontend/src/components/NavBar.vue
+++ shiftly-frontend/src/components/NavBar.vue
@@ -19,7 +19,7 @@
 
     <!-- Navigation -->
     <v-list density="compact" nav>
-      <v-list-item prepend-icon="mdi-home" title="Dashboard" value="myfiles"></v-list-item>
+      <v-list-item prepend-icon="mdi-home" title="Dashboard" :to="{ name: 'about' }"></v-list-item>
       <v-list-item prepend-icon="mdi-account-multiple" title="Shared with me" value="shared"></v-list-item>
       <v-list-item prepend-icon="mdi-star" title="Starred" value="starred"></v-list-item>
     </v-list>
