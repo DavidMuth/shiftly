de164e3ab267fa7931daad0f2a02854e5b47f517
MrBierflasche <schmidtmoritz143@gmail.com>
2025-12-04 13:59:34 +0100

    UserService und User store hinzugefügt

---
+++ shiftly-frontend/src/stores/User.ts
@@ -0,0 +1,28 @@
+import { defineStore } from 'pinia';
+import type { User} from '@/types/User';
+import UserService from '@/services/UserService';
+
+interface UserState {
+  user: User | null;
+}
+
+export const useUserStore = defineStore('user', {
+  state: (): UserState => ({
+    user: null,
+  }),
+
+  getters: {
+    getUser: (state): User | null => state.user,
+  },
+
+  actions: {
+    async getCurrentUser(): Promise<void> {
+    try {
+      const response = await UserService.getCurrentUser();
+      this.user = response.data;
+    } catch (error) {
+      console.error("Login failed:", error);
+    }
+  }
+ }
+});
